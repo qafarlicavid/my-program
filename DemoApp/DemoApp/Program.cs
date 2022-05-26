5c5a19b436316551277a4704c3db68bce78eb333
qafarlicavid <javidig@code.edu.az>
2022-05-26 10:47:59 -0700

    New project already added

---
+++ DemoApp/DemoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cavidSalary = 1700;
+            int resulSalary = 2000;
+            int elminSalary = 2400;
+            int ceyhunSalary = 1400;
+
+            int totalSalary = cavidSalary + resulSalary + elminSalary + ceyhunSalary;
+
+            int zekat = totalSalary / 25;
+            Console.WriteLine(zekat);
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
